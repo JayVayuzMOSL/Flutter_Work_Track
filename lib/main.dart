343218bc32c84d66b28078aab226c889e7a32d33
jaykadam <jay.kadam@vayuz.in>
2025-02-13 12:11:39 +0530

    changes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_work_track/app.dart';
-import 'package:flutter_work_track/service_locator.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'package:eghlflutter/eghlflutter.dart';
 
-  await Hive.initFlutter();
+void main() {
+  runApp(const MyApp());
+}
 
-  await setupLocator();
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  runApp(const WorkTrackApp());
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _eghlPaymentResult = 'Awaiting for payment.';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // ignore: non_constant_identifier_names
+  void _Pay(BuildContext context) async {
+    String result = '';
+    String paymentId = 'SIT${DateTime.now().millisecondsSinceEpoch}';
+
+    try {
+      Map<String, dynamic> payment = {
+        'TransactionType': 'SALE',
+        'Amount': '1.00',
+        'CurrencyCode': 'MYR',
+        'PaymentId': paymentId,
+        'OrderNumber': paymentId,
+        'PaymentDesc': 'Testing Payment',
+        'PymtMethod': 'ANY',
+        'CustName': 'somebody',
+        'CustEmail': 'somebody@someone.com',
+        'CustPhone': '0123456789',
+        'MerchantReturnURL': 'SDK',
+        'MerchantCallBackURL': 'SDK',
+        'ServiceId': 'XSC',
+        'Password': 'xsc12345',
+        'LanguageCode': 'EN',
+        'PageTimeout': '600',
+        'PaymentGateway': true,
+        'EnableCardPage': false,
+        'TriggerReturnURL': false,
+        'WebViewZoom': false,
+        'NumOfRequery': 12,
+        'ForceClosePayment': true,
+        'Loggable': true,
+      };
+
+      result = await Eghlflutter.executePayment(payment);
+    } catch (e) {
+      // result = e.message;
+    }
+
+    setState(() {
+      _eghlPaymentResult = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_eghlPaymentResult),
+              ElevatedButton(
+                child: const Text("Pay"),
+                onPressed: () {
+                  _Pay(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
