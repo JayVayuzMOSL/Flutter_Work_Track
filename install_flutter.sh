a405d40f4c693745076c41b2b20b0fa32e39a1de
jaykadam <jay.kadam@vayuz.in>
2025-02-13 13:24:21 +0530

    netlify changes

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e
+# Setup flutter
+FLUTTER=`which flutter`
+if [ $? -eq 0 ]
+then
+  # Flutter is installed
+  FLUTTER=`which flutter`
+else
+  # Get flutter
+  git clone https://github.com/flutter/flutter.git
+  FLUTTER=flutter/bin/flutter
+fi
 
-# Define Flutter version (change this if needed)
-FLUTTER_VERSION="3.19.0"
-FLUTTER_ARCHIVE="flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
-FLUTTER_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/${FLUTTER_ARCHIVE}"
+# Configure flutter
+FLUTTER_CHANNEL=beta
+FLUTTER_VERSION=v1.17.0
+$FLUTTER channel $FLUTTER_CHANNEL
+$FLUTTER version $FLUTTER_VERSION
+$FLUTTER config --enable-web
 
-# Install dependencies
-echo "Installing dependencies..."
-sudo apt-get update
-sudo apt-get install -y curl unzip xz-utils git
+# Setup dart
+DART=`echo $FLUTTER | sed 's/flutter$/cache\/dart-sdk\/bin\/dart/'`
+echo $DART
 
-# Download Flutter SDK
-echo "Downloading Flutter SDK..."
-curl -o $FLUTTER_ARCHIVE $FLUTTER_URL
+# e.g. Run a dart command
+$DART foo/bar.dart
 
-# Extract Flutter
-echo "Extracting Flutter..."
-tar -xf $FLUTTER_ARCHIVE
+# Build flutter for web
+$FLUTTER build web
 
-# Move Flutter to the correct location
-export PATH="$PWD/flutter/bin:$PATH"
-
-# Verify Flutter installation
-echo "Flutter version:"
-flutter --version
-
-# Accept Flutter licenses
-echo "Accepting Flutter licenses..."
-yes | flutter doctor --android-licenses || true
-
-# Run Flutter Doctor
-flutter doctor
-
-echo "Flutter installation completed successfully!"
+echo "OK"
